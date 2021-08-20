9335030b36f184b96623edb3542bdb3ef994c7d3
Patrick Boivin <pboivin@gmail.com>
2021-08-20 12:49:44 -0400

    WIP

--- .auto/bump-package-version.js
+++ .auto/bump-package-version.js
@@ -1,4 +1,13 @@
-// const fs = require('fs')
+const core = require("@auto-it/core")
+
+const fs = require("fs")
+const promisify = require("util").promisify
+
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
+
+const PACKAGE_JSON_PATH = "./package.json"
+const SERVICE_PROVIDER_PATH = "./src/TwillServiceProvider.php"
 
 module.exports = class BumpPackageVersion {
   constructor() {
@@ -7,15 +16,32 @@ module.exports = class BumpPackageVersion {
 
   apply(auto) {
     auto.hooks.version.tapPromise("BumpPackageVersion", async ({ bump, dryRun, quiet }) => {
-      console.log('BUMP = ' + bump)
+      if (dryRun) return
 
-      // fs.writeFile('BUMP.txt', bump)
+      let packageJson = JSON.parse(await readFile(PACKAGE_JSON_PATH, "utf-8"))
+      const nextVersion = inc(packageJson.version, bump)
 
-      // if (dryRun) {
-      //   const { version } = await loadPackageJson()
-      //   console.log(inc(version, bump))
-      //   return
-      // }
+      if (!Boolean(nextVersion)) return
+      if (`${packageJson.version}` === `${nextVersion}`) return
+
+      /**
+       * Update package.json version
+       */
+      packageJson.version = nextVersion
+      await writeFile(JSON.stringify(packageJson, null, "  "), PACKAGE_JSON_PATH, "utf-8")
+
+      /**
+       * Update TwillServiceProvider version
+       */
+      let serviceProvider = await readFile(SERVICE_PROVIDER_PATH, "utf-8")
+      serviceProvider.replace(/    const VERSION = '.*';/, `    const VERSION='${nextVersion}';`)
+      await writeFile(serviceProvider, SERVICE_PROVIDER_PATH, "utf-8")
+
+      /**
+       * Push changes
+       */
+      await core.execPromise("git", ["commit", "-am", "Bump version [skip ci]"]);
+      await core.execPromise("git", ["push", "--all"]);
     })
   }
 }
